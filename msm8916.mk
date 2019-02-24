0d281d5c93ecd9c78cb40a722794362781b58355
Kyle Harrison <khwebmail@gmail.com>
2019-02-24 12:23:30 +0000

    Msm8916-common: Add IMS stack All Credits Goes to @Nicorg2515

--- msm8916.mk
+++ msm8916.mk
@@ -184,6 +184,7 @@ PRODUCT_PACKAGES += \
 
 # Telephony
 PRODUCT_PACKAGES += \
+    ims-ext-common \
     telephony-ext
 
 PRODUCT_BOOT_JARS += telephony-ext
@@ -235,6 +236,10 @@ PRODUCT_PACKAGES += \
     libOmxVenc \
     libOmxVidcCommon \
     libstagefrighthw
+
+# Qualcomm
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/privapp-permissions-qti.xml
 
 # Ramdisk
 PRODUCT_PACKAGES += \
