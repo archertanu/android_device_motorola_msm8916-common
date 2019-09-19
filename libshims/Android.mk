b6d3a9abf44b92e99a2ce0d0ce1e1fc6c910ef94
Archer <tanumoydhali@gmail.com>
2019-09-19 16:02:21 +0530

    Remove current GPS Libshim

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -23,14 +23,6 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_VENDOR_MODULE := true
 include $(BUILD_SHARED_LIBRARY)
 
-# GPS
-include $(CLEAR_VARS)
-LOCAL_SRC_FILES := get_process_name.c
-LOCAL_MODULE := libshims_get_process_name
-LOCAL_MODULE_TAGS := optional
-LOCAL_VENDOR_MODULE := true
-include $(BUILD_SHARED_LIBRARY)
-
 # IMS
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := MediaBuffer.cpp
